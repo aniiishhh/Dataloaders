4e83edb2149c587ee461fdddbf73f5b53e1cacb7
aniiishhh <116051368+aniiishhh@users.noreply.github.com>
2023-12-25 14:03:15 -0800

    Initial Commit

---
+++ T2I-Task/T2IDataset.py
@@ -0,0 +1,29 @@
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+import os
+import pandas as pd
+
+
+class T2I_Dataset(Dataset):
+    def __init__(self, data_dir, transforms=None):
+        self.data_dir = data_dir
+        self.captions = pd.read_csv(os.path.join(data_dir, "captions.csv"))
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.captions)
+
+    def __getitem__(self, idx):
+        img_filename = str(self.captions.iloc[idx, 0]) + ".jpg"
+        img_path = os.path.join(self.data_dir, "Images", img_filename)
+        image = read_image(img_path)
+        caption = self.captions.iloc[idx, 1]
+
+        # if self.transforms:
+        #     image = self.transforms(image)
+
+        return caption, image
+
+
+if __name__ == "__main__":
+    print(T2I_Dataset("Data").__getitem__(1))

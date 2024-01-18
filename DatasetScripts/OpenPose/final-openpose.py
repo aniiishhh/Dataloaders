df491992ac7351f2bd60e99c13cb7850ff00a33c
aniiishhh <116051368+aniiishhh@users.noreply.github.com>
2024-01-17 23:45:28 -0700

    Add Openpose

---
+++ DatasetScripts/OpenPose/final-openpose.py
@@ -0,0 +1,56 @@
+"""
+Execute using:
+    python final-openpose.py --i <Input Folder> --o <Output Folder> --cuda
+"""
+
+import argparse
+from controlnet_aux import OpenposeDetector
+from diffusers.utils import load_image
+import numpy as np
+import cv2
+import os
+import torch
+
+
+def process_images(input_folder, output_folder, use_cuda):
+    device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
+
+    openpose = OpenposeDetector.from_pretrained("lllyasviel/ControlNet").to(device)
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    for filename in os.listdir(input_folder):
+        if filename.endswith((".jpg", ".png", ".jpeg")):
+            image_path = os.path.join(input_folder, filename)
+            image = load_image(image_path)
+
+            image_np = np.array(image)
+
+            image_tensor = torch.tensor(image_np).to(device)
+
+            processed_image = openpose(image_tensor)
+
+            filename = filename.replace(".", "_openpose.")
+            output_path = os.path.join(output_folder, filename)
+            processed_image.save(output_path)
+            print(f"Successfully written {filename}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Batch process images using Openpose")
+    parser.add_argument(
+        "--i",
+        dest="input_folder",
+        required=True,
+        help="Input folder containing images",
+    )
+    parser.add_argument(
+        "--o",
+        dest="output_folder",
+        default="Output",
+        help="Output folder for processed images",
+    )
+    parser.add_argument("--cuda", action="store_true", help="Use CUDA if available")
+    args = parser.parse_args()
+    process_images(args.input_folder, args.output_folder, use_cuda=args.cuda)
+    print("-" * 150)
